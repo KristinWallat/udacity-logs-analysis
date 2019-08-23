60e792cc891c6f0ec1a8ee2caaae06ac0c51a8e6
Kristin Wallat <42810352+KristinWallat@users.noreply.github.com>
2019-08-23 15:05:40 +0200

    Remove whitespace

--- logsanalysis.py
+++ logsanalysis.py
@@ -57,7 +57,6 @@ def get_errors():
     print("\nOn this day more than 1% of requests led to an error:")
     for k in c_records:
             print(str(k[0]) + "%" + "--" + str(k[1]))
-
             
 get_popular_articles(3)
 get_popular_authors()
